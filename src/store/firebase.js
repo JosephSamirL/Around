97cbcf41f2cec23188b68ab7c7397c796aba4255
J o s e p h  S a m i r <68662601+josephcmd@users.noreply.github.com>
2021-02-07 18:26:17 +0200

    Update firebase.js

--- src/store/firebase.js
+++ src/store/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database"
 const firebaseConfig = {
-    apiKey: "AIzaSyAP2sFz_Ctj0cflaL37Qcv6C44pmAkAjso",
-    authDomain: "around-1ede8.firebaseapp.com",
-    projectId: "around-1ede8",
-    storageBucket: "around-1ede8.appspot.com",
-    messagingSenderId: "95680089866",
-    appId: "1:95680089866:web:f83f6bffda997118b134f3"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   firebase.initializeApp(firebaseConfig)
 export const provider = new firebase.auth.GoogleAuthProvider();  
